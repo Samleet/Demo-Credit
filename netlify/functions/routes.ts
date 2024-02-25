13ed5a9c3b2cead8988790bbbb2a92993cb844ad
Samleet <tellusyourproblem11@gmail.com>
2024-02-25 19:33:48 +0100

    Netlify: routes redirects setup

--- netlify/functions/routes.ts
+++ netlify/functions/routes.ts
@@ -1,8 +1,7 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
 import express, { Router } from "express";
 import serverless from "serverless-http";
 import routes from '../../src/routes/index';
+
 
 const api = express();
 
