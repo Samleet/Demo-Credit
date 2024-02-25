4722da39bd3edb4ff38bf392020a44c0ff54159a
Samleet <tellusyourproblem11@gmail.com>
2024-02-25 21:33:11 +0100

    Netlify: fixed view resolving

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -6,25 +6,5 @@ import session from "express-session";
 import app from "../../app"
 
 
-app.use(express.json());
-app.use(express.static('public'));
-app.use(express.urlencoded());
-app.use(
-  session({
-    secret: "demoCredit",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000
-    }
-  })
-)
-
-app.set('view engine', 'html');
-app.set('views', '../../src/views');
-app.engine('html', ejs.renderFile);
-
-app.use(routes);
-
+app.set('views', 'views');
 export const handler = serverless(app);
