379597b5a9b77625598792e0198e73ac80b16f65
Samleet <tellusyourproblem11@gmail.com>
2024-02-26 07:30:14 +0100

    Render: attempt to host build

--- build/netlify/functions/api.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
-import ejs from 'ejs';
-import routes from '../../src/routes/index';
-import session from "express-session";
-import app from "../../app"
-
-
-export const handler = serverless(app);
